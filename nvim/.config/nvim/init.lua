7fcbade30d15d1f8aac043f54b43ab4e661878bd
taeyoung kim <95904582+kimpossible-TY@users.noreply.github.com>
2026-05-24 18:00:56 +0900

    update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,6 +13,22 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- 시스템 클립보드 사용 설정
+vim.opt.clipboard = "unnamedplus"
+
+-- Codespaces(SSH) 등 원격 환경에서 로컬 Windows와 클립보드 연동 (OSC 52)
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 -- 기본적인 VIM 셋팅
 vim.g.mapleader = " "
 vim.opt.number = true
@@ -27,6 +43,22 @@ vim.opt.smartcase = true
 vim.opt.cursorline = true
 vim.opt.laststatus = 3
 vim.opt.sidescrolloff = 8  -- 커서 좌우에 최소 8글자 여유를 둠
+
+-- 시스템 클립보드 사용 설정
+vim.opt.clipboard = "unnamedplus"
+
+-- Codespaces(SSH) 등 원격 환경에서 로컬 Windows와 클립보드 연동 (OSC 52)
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
 
 if vim.treesitter.language and not vim.treesitter.language.ft_to_lang then
   vim.treesitter.language.ft_to_lang = function(ft)
